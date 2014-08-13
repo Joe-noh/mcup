278b95622504cfe56daa6e390d02c87f577994af
Joe-noh <goflb.jh@gmail.com>
2014-08-14 03:49:11 +0900

    publish!

--- mix.exs
+++ mix.exs
@@ -2,29 +2,36 @@ defmodule Mcup.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mcup,
-     version: "0.0.1",
-     elixir: "~> 0.15.1-dev",
-     deps: deps]
+    [
+      app: :mcup,
+      version: "0.0.1",
+      elixir: "~> 0.15.0",
+      description: desc,
+      package: package,
+      deps: deps
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp desc do
+    """
+    DSL for markup.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Joe Honzawa"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Joe-noh/mcup"}
+    ]
   end
 end
